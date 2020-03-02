a335338aec35f594b32ccabda86646a8ec28e786
FelipeAmorimFerreira <felipe.amorim.ferreira@ccc.ufcg.edu.br>
2020-03-02 18:24:00 -0300

    Updates

---
+++ precache-manifest.b4ea60bbeaffb130b0bc4a27ad1cd344.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3dd92eb7e30a1eb269b3535fca6fa7",
+    "url": "/simple-react-routing/index.html"
+  },
+  {
+    "revision": "b5fec7c4f9e2e0957d4c",
+    "url": "/simple-react-routing/static/css/main.d5efd0ea.chunk.css"
+  },
+  {
+    "revision": "ba66e1ddc77c3a2aa4f7",
+    "url": "/simple-react-routing/static/js/2.f77f0eb2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/simple-react-routing/static/js/2.f77f0eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5fec7c4f9e2e0957d4c",
+    "url": "/simple-react-routing/static/js/main.dd1e683d.chunk.js"
+  },
+  {
+    "revision": "4c8eae272d357c740a47",
+    "url": "/simple-react-routing/static/js/runtime-main.1c5e2f26.js"
+  }
+]);
