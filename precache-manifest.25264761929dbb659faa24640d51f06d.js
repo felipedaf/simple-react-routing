0d834118a428670c1595ae4a6c51ed0788503789
FelipeAmorimFerreira <felipe.amorim.ferreira@ccc.ufcg.edu.br>
2020-03-23 11:05:10 -0300

    Updates

---
+++ precache-manifest.25264761929dbb659faa24640d51f06d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2aa140bc05ca3b2cfd7a992737a2b85",
+    "url": "/simple-react-routing/index.html"
+  },
+  {
+    "revision": "32d6bd7810b6471bfef1",
+    "url": "/simple-react-routing/static/css/main.e54ca922.chunk.css"
+  },
+  {
+    "revision": "bbb63de33eb6607bb14e",
+    "url": "/simple-react-routing/static/js/2.c737a48d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/simple-react-routing/static/js/2.c737a48d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d6bd7810b6471bfef1",
+    "url": "/simple-react-routing/static/js/main.5275662c.chunk.js"
+  },
+  {
+    "revision": "4c8eae272d357c740a47",
+    "url": "/simple-react-routing/static/js/runtime-main.1c5e2f26.js"
+  },
+  {
+    "revision": "d9a2f7375a422f8462957993880ffa16",
+    "url": "/simple-react-routing/static/media/loading.d9a2f737.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-react-routing/precache-manifest.14916dfd5019890e9a838c9de62dcd2b.js"
+  "/simple-react-routing/precache-manifest.25264761929dbb659faa24640d51f06d.js"
 );
 
 self.addEventListener('message', (event) => {
